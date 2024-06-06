75c570f0d41a38c04e5c399b8cdb317a407407a5
Srikant Dhoundiyal <srikant_008@live.com>
2024-06-06 11:57:14 +0530

    base point 01

---
+++ app.py
@@ -0,0 +1,11 @@
+print("helooo this is first version 2.0")
+from datetime import datetime
+
+# Get the current time
+current_time = datetime.now()
+
+# Format the current time
+formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
+
+# Print the current time
+print("Current Time:", formatted_time)
